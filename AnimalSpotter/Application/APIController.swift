5f8de98f41e0288fea32782b6da4f985c42c1d1e
Tobi Tselios <tobi.tselios@gmail.com>
2020-01-15 14:05:01 +0000

    Sign up function done

--- AnimalSpotter/Application/APIController.swift
+++ AnimalSpotter/Application/APIController.swift
@@ -20,6 +20,58 @@ class APIController {
     
     // create function for sign up
     
+    func signUp(with user: User, completion: @escaping (Error?) -> Void) {
+        
+        // Build the URL
+        
+        let requestURL = baseUrl
+            .appendingPathComponent("users")
+            .appendingPathComponent("signup")
+        
+        // Build the request
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        
+        // Tell the API that the body is in JSON format
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let encoder = JSONEncoder()
+        
+        do {
+            let userJSON = try encoder.encode(user)
+            request.httpBody = userJSON
+        } catch {
+            NSLog("Error encoding user object: \(error)")
+        }
+        
+        // Perform the request (data task)
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            // Handle errors
+            if let error = error {
+                NSLog("Error signing user: \(error)")
+                completion(error)
+            }
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                
+                let statusCode = NSError(domain: "com.TobiKuyoro.AnimalSpotter", code: response.statusCode, userInfo: nil)
+                completion(statusCode)
+            }
+            
+            // nil means there was no error. Everything succeeded.
+            completion(nil)
+        }.resume()
+        
+        
+        
+        // Decoded the data (optionally)
+        
+    }
+    
     // create function for sign in
     
     // create function for fetching all animal names
