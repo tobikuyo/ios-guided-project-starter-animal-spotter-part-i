0c60b305055533bb04e491e4496cedf9afd84c42
Tobi Tselios <tobi.tselios@gmail.com>
2020-01-15 14:48:10 +0000

    Sign in done

--- AnimalSpotter/Application/APIController.swift
+++ AnimalSpotter/Application/APIController.swift
@@ -18,7 +18,9 @@ class APIController {
     
     private let baseUrl = URL(string: "https://lambdaanimalspotter.vapor.cloud/api")!
     
-    // create function for sign up
+    var bearer: Bearer?
+    
+    // Create function for sign up
     
     func signUp(with user: User, completion: @escaping (Error?) -> Void) {
         
@@ -73,6 +75,56 @@ class APIController {
     }
     
     // create function for sign in
+    
+    func signIn(with user: User, completion: @escaping (Error?) -> Void) {
+        let requestURL = baseUrl
+            .appendingPathComponent("users")
+            .appendingPathComponent("login")
+        
+        var request = URLRequest(url: requestURL)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = HTTPMethod.post.rawValue
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(user)
+        } catch {
+            NSLog("Error encoding user for sign in: \(error)")
+            completion(error)
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            if let error = error {
+                NSLog("Error signing in user: \(error)")
+                completion(error)
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse ,
+                response.statusCode != 200 {
+                
+                let statusCodeError = NSError(domain: "com.TobiKuyoro.AnimalSpotter", code: response.statusCode, userInfo: nil)
+                completion(statusCodeError)
+            }
+            
+            guard let data = data else {
+                NSLog("no data returned from data task")
+                let noDataError = NSError(domain: "com.TobiKuyoro.AnimalSpotter", code: -1, userInfo: nil)
+                completion(noDataError)
+                return
+            }
+            
+            do {
+                let bearer = try JSONDecoder().decode(Bearer.self, from: data)
+                self.bearer = bearer
+            } catch {
+                NSLog("Error decoding the bearer token: \(error)")
+                completion(error)
+            }
+            
+            completion(nil)
+        }.resume()
+    }
     
     // create function for fetching all animal names
     
---
+++ AnimalSpotter/Models/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  AnimalSpotter
+//
+//  Created by Tobi Kuyoro on 15/01/2020.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
