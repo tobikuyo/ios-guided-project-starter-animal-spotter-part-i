961ccc94d7782f70ae71dc290ebd55e3efb6c783
Tobi Tselios <tobi.tselios@gmail.com>
2020-01-15 14:25:11 +0000

    Sign up done

--- AnimalSpotter/View Controllers/LoginViewController.swift
+++ AnimalSpotter/View Controllers/LoginViewController.swift
@@ -34,10 +34,53 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     // MARK: - Action Handlers
     
     @IBAction func buttonTapped(_ sender: UIButton) {
+        // Create a user
+        guard let username = usernameTextField.text,
+            let password = passwordTextField.text,
+            !username.isEmpty,
+            !password.isEmpty else { return }
+        
+        let user = User(username: username, password: password)
+        
         // perform login or sign up operation based on loginType
+        
+        if loginType == .signUp {
+            signUp(with: user)
+        } else {
+            
+        }
+    }
+    
+    func signUp(with user: User) {
+        apiController?.signUp(with: user, completion: { (error) in
+            
+            if let error = error {
+                NSLog("Error occured during sign up: \(error)")
+            } else {
+                let alert = UIAlertController(title: "Sign Up Successful",
+                                              message: "Now please log in",
+                                              preferredStyle: .alert)
+                
+                let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(okAction)
+                
+                self.present(alert, animated: true) {
+                    self.loginType = .signIn
+                    self.loginTypeSegmentedControl.selectedSegmentIndex = 1
+                    self.signInButton.setTitle("Sign In", for: .normal)
+                }
+            }
+        })
     }
     
     @IBAction func signInTypeChanged(_ sender: UISegmentedControl) {
-        // switch UI between login types
+
+        if sender.selectedSegmentIndex == 0 {
+            loginType = .signUp
+            signInButton.setTitle("Sign Up", for: .normal)
+        } else {
+            loginType = .signIn
+            signInButton.setTitle("Sign In", for: .normal)
+        }
     }
 }
